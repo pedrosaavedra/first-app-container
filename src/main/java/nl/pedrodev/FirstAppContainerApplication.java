df47b27dff0d8f0a916eba0c603ebf84f610da43
Pedro Saavedra <pedrosaavedraemail@gmail.com>
2023-09-09 11:00:10 +0200

    First commit

---
+++ src/main/java/nl/pedrodev/FirstAppContainerApplication.java
@@ -0,0 +1,13 @@
+package nl.pedrodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppContainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/pedrodev/FirstAppContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.pedrodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
